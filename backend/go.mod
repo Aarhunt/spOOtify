c57eb3d2f845f187490eed1bb2cc9f88b08ed17a
Arend <19819601+Aarhunt@users.noreply.github.com>
2026-01-08 12:11:42 +0100

    feat(backend): Added postgres connection

--- backend/go.mod
+++ backend/go.mod
@@ -26,6 +26,10 @@ require (
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -61,6 +65,7 @@ require (
 	google.golang.org/protobuf v1.36.9 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.6.0 // indirect
+	gorm.io/driver/postgres v1.6.0 // indirect
 	gorm.io/driver/sqlite v1.6.0 // indirect
 	gorm.io/gorm v1.31.1 // indirect
 )
--- backend/src/db.go
+++ backend/src/db.go
@@ -3,11 +3,13 @@ package src
 import (
 	"context"
 	"fmt"
+	"os"
 	"sync"
 
+	"github.com/aarhunt/spootify/src/model"
+	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"github.com/aarhunt/spootify/src/model"
 )
 
 var lockDbConn = &sync.Mutex{}
@@ -22,10 +24,30 @@ var dbConnInstance *dbConn
 func createDbConn() *dbConn{
 	ctx := context.Background()
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
-	  if err != nil {
-		panic("failed to connect database")
-	  }
+	pgConnString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable",
+		os.Getenv("PGHOST"),
+		os.Getenv("PGPORT"),
+		os.Getenv("PGDATABASE"),
+		os.Getenv("PGUSER"),
+		os.Getenv("PGPASSWORD"),
+	)
+
+	env := os.Getenv("NODE_ENV")
+	fmt.Println(env)
+
+	var db *gorm.DB
+	var err error
+	if env == "production" {
+		db, err = gorm.Open(postgres.Open(pgConnString), &gorm.Config{})
+		  if err != nil {
+			panic("failed to connect database")
+		  }
+	} else if env == "development" {
+		db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+		  if err != nil {
+			panic("failed to connect database")
+		  }
+	}
 
 	db.AutoMigrate(&model.Playlist{})
 
